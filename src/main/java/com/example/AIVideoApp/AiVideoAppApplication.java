ae97ea72cb805a7c02ec75d65d40faa8ac568933
seokwon cho <dkf1141@naver.com>
2025-02-15 20:00:22 +0900

    파일 초기화

---
+++ src/main/java/com/example/AIVideoApp/AiVideoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AIVideoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiVideoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiVideoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AIVideoApp/AiVideoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIVideoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiVideoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
